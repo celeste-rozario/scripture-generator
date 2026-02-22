502a7ac4f464c686f081e2be605916fea8756e29
celeste-rozario <celestearcosmeza@gmail.com>
2026-02-22 16:44:39 +1100

    Added IDs in HTML and javascript functionality of the form plus the typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("Hello there, ready for scripture to come alive?");
+
+function generateScripture(event){
+    event.preventDefault();
+
+    new Typewriter("#scripture", {
+        strings: ["Scripture will go here"],
+        autoStart: true,
+        delay:1, 
+        cursor: "",
+      });
+
+}
+
+
+let emotionInput = document.querySelector("#scripture-generator-form");
+emotionInput.addEventListener("submit", generateScripture)
