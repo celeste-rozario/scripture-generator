d65869de9349f0e97218924028fd4d3eb86e44cc
celeste-rozario <celestearcosmeza@gmail.com>
2026-02-23 07:50:45 +1100

    Improved the user experience
    Corrected some spelling mistakes
    Added a hidden element when page in default
    Added a generating animation

--- src/index.js
+++ src/index.js
@@ -14,12 +14,16 @@ function displayScripture(response){
 function generateScripture(event){
     event.preventDefault();
 
-    let emotionInput = document.querySelector("#promop-input-bar");
+    let emotionInput = document.querySelector("#prompt-input-bar");
     let apiKey = "9b5e5489a22c2ct5203a0ad09d7f3oab";
     let context = "You know the christian bible well. Generate scripture and format it so that every new sentence is a new line. Make sure to follow the user instructions ";
     let prompt  = `User Instructions: Generate one scripture from the Christian bible to encourage whatever the user put in ${emotionInput} only generate scripture and no other words. Makre sure the chapter and verse is in <strong> and displayed at the bottom of the scripture on a new line below the scripture`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let hiddenElement = document.querySelector("#scripture");
+    hiddenElement.classList.remove("hidden");
+    hiddenElement.innerHTML = `<div class="generating"> ⏳ A scripture about being ${emotionInput.value} is generating</div>`;
+    
     axios.get(apiUrl).then(displayScripture);
 
 }
